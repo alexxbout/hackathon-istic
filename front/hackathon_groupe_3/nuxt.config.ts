74fa5a821e07c7fb09202018e3ff6e760a26a1d9
anhnguyen <anguyen@blueway.fr>
2025-02-18 12:09:29 +0100

    chore: config

--- front/hackathon_groupe_3/nuxt.config.ts
+++ front/hackathon_groupe_3/nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  css: ['./public/assets/css/main.css'],
+  ui: {
+    devtools: {
+      enabled: false
+    },
+    colors: {
+      primary: 'gray',
+      secondary: 'green',
+      info: '#41B883',
+      success: '#41B883',
+      warning: '#E6A23C',
+      error: '#F56C6C',
+      neutral: '#35495E'
+    },
+
+  }
 })
