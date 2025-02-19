3b533044786f45c4cbd105c281c543c3e9cf497d
Berkaoui Meryem <meryem.berkaoui@etudiant.univ-rennes.fr>
2025-02-19 16:40:00 +0100

    Pool Modal mais action modifier non défini + controle sur compétences

--- front/hackathon_groupe_3/pages/pools.vue
+++ front/hackathon_groupe_3/pages/pools.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="bg-red-400 h-20 w-20">
-    
+  <div>
+    <UButton label="Open" @click="openModal" />
+
+    <PoolModal v-model="modal" />
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+import { ref } from "vue";
+import PoolModal from "~/components/PoolModal.vue";
+
+const modal = ref(false);
+
+const openModal = () => {
+  modal.value = true;
+};
 
 </script>
-
-<style>
-
-</style>
