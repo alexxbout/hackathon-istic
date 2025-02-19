5cdd35c947fe5d6adc09d238631712a2ec224908
alexxbout <boutinaud.pro@gmail.com>
2025-02-19 15:18:43 +0100

    small changes

--- front/hackathon_groupe_3/components/SearchBar.vue
+++ front/hackathon_groupe_3/components/SearchBar.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="border border-black rounded-2xl flex items-center justify-center w-full h-max p-3 gap-x-2.5 shrink-0">
-        <USelectMenu @change="handleSkillsUpdate" class="w-full" variant="outline" color="gray" size="xl" v-model="selectedSkills" :options="skillOptions" :placeholder="selectSkillPlaceholder" searchable :searchable-placeholder="skillSearchText" multiple>
+    <div class="border border-black rounded-2xl flex items-center justify-center w-full h-max p-3 gap-x-2.5">
+        <USelectMenu @change="handleSkillsUpdate" class="w-full" variant="outline" color="gray" size="md" v-model="selectedSkills" :options="skillOptions" :placeholder="selectSkillPlaceholder" searchable :searchable-placeholder="skillSearchText" multiple>
             <template #label>
                 <span v-if="selectedSkills.length" class="truncate">{{ selectedSkillLabels }}</span>
                 <span v-else>{{ selectSkillPlaceholder }}</span>
@@ -11,11 +11,11 @@
             </template>
         </USelectMenu>
 
-        <USelect @change="handleExperienceUpdate" class="w-full" variant="outline" color="gray" size="xl" v-model="selectedExperience" :options="experiencesOptions" :placeholder="experiencePlaceholder" />
+        <USelect @change="handleExperienceUpdate" class="w-full" variant="outline" color="gray" size="md" v-model="selectedExperience" :options="experiencesOptions" :placeholder="experiencePlaceholder" />
 
         <div v-if="props.role != 'rh'" class="w-full">
-            <UPopover :popper="{ placement: 'bottom-start' }">
-                <UButton size="xl" icon="i-heroicons-calendar-days-20-solid"> {{ format(selectedPeriod.start, "d MMM, yyy") }} - {{ format(selectedPeriod.end, "d MMM, yyy") }}</UButton>
+            <UPopover class="w-full" :popper="{ placement: 'bottom-start' }">
+                <UButton size="md" icon="i-heroicons-calendar-days-20-solid"> {{ format(selectedPeriod.start, "d MMM, yyy") }} - {{ format(selectedPeriod.end, "d MMM, yyy") }}</UButton>
 
                 <template #panel="{ close }">
                     <div class="flex items-center sm:divide-x divide-gray-200 dark:divide-gray-800">
