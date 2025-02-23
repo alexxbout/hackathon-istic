e7e926e88890bca8ef543e1c9de712c0d083812a
alexxbout <boutinaud.pro@gmail.com>
2025-02-23 18:16:34 +0100

    rollback backend-service

--- front/hackathon_groupe_3/nuxt.config.ts
+++ front/hackathon_groupe_3/nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     },
     routeRules: {
         "/api/v1/**": {
-            proxy: { to: "http://backend:8080/api/v1/**" },
+            proxy: { to: "http://backend-service:8080/api/v1/**" },
         },
     }
 });
