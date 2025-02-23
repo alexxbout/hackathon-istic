22207df533047dc18a4a979f1af4b1a8b6049271
Pierre Edouard <pierreedouardhermenier@gmail.com>
2025-02-23 12:23:09 +0100

    deploy auth

--- back/auth/src/main/java/fr/istic/auth/controller/UserController.java
+++ back/auth/src/main/java/fr/istic/auth/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("/api/users")
 public class UserController {
 
+    //UserService
     private final UserService userService;
 
     public UserController(UserService userService) {
